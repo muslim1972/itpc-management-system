bf70cc3625e8ef2e6c2964be0519de1963ded29c
muslim1972 <muslimakkeel@gmail.com>
2026-04-17 11:02:21 +0300

    بعونه سبحانه وتعالى تحسين العرض على الموبايل (4) 17/4/2026 11:00 ص

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -181,7 +181,9 @@ const OrganizationsSection = () => {
           }}
           className="btn-primary flex items-center justify-center gap-2 px-4 py-2 text-sm"
         >
-          <Plus className="h-4 w-4" />
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clipRule="evenodd" />
+          </svg>
           <span>إضافة جهة</span>
         </button>
       </div>
@@ -581,7 +583,9 @@ const CompaniesSection = ({ onDetails }) => {
           }}
           className="btn-primary flex items-center justify-center gap-2 px-4 py-2 text-sm"
         >
-          <Plus className="h-4 w-4" />
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clipRule="evenodd" />
+          </svg>
           <span>إضافة شركة</span>
         </button>
       </div>
